5f7e24d6bfee8351025bc201dfdf9d843f777bf4
denizAM <deniz.a.marin@gmail.com>
2024-02-23 20:25:44 -0600

    Cambios2 - error en /dist

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import 'vite/modulepreload-polyfill'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+  open: '/docs/index.html',
+  }, 
+  server: {
+    // This is the default value, and will add all files with node_modules
+    // in their paths to the ignore list.
+    sourcemapIgnoreList(sourcePath, sourcemapPath) {
+      return sourcePath.includes('node_modules')
+    }
+  }
 })
